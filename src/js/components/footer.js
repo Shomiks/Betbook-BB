0b8f405b6cbc8e1ccd025fc96edde7f06f68ecf1
dusanugrcic <d.ugrcic@nix.mk>
2019-09-04 13:38:28 +0200

    Revert "screens"
    This reverts commit e3c4a497e82820fc56068b040eb534bf4f09f20d.

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -5,7 +5,22 @@ import {Link} from "react-router-dom";
  class Footer extends React.Component {
    constructor(props) {
      super(props);
+     this.state = {
+         timeline: false,
+         ball: false,
+         star: false,
+         profile: false
+     };
      this.sharedObj = props.sharedObj;
+   }
+
+     toggleClass(timeline) {
+         const currentState = this.state.active;
+         this.setState({ timeline: !currentState });
+     };
+
+   onClickAddActive = (className) => {
+       return className + ' active'
    }
 
    render() {
@@ -14,10 +29,15 @@ import {Link} from "react-router-dom";
      return (
 
          <div className='rectangle_footer'>
-             <Link to={`/home`}> <div className='timeline'/></Link>
-             <Link to={`/countries`}><div className='ball'/></Link>
-           <div className='star'/>
-             <Link to={`/profile`}><div className='profile'></div></Link>
+             <div className='ft_home-field'>
+                 <Link to={`/home`}> <div className={this.state.active ? 'timeline active' : 'timeline'} onClick={() => this.toggleClass('timeline')}><span>{this.state.timeline ? 'Home' : ''}</span></div></Link>
+             </div>
+             <div className='ft_countries-field'><Link to={`/countries`}><div className='ball'/></Link>
+             </div>
+             <div className='ft_star-field'><div className='star'/>
+             </div>
+             <div className='ft_profile-field'><Link to={`/profile`}><div className='profile'/><span className='text10-white'>Profile</span></Link>
+             </div>
          </div>
      )
    }
