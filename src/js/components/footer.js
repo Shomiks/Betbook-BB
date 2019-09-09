9489dcf20432f0fe500a496fbf93380bded51827
dusanugrcic <d.ugrcic@nix.mk>
2019-09-09 13:56:08 +0200

    Revert "screens"
    This reverts commit e3c4a497e82820fc56068b040eb534bf4f09f20d.

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -23,19 +23,19 @@ import {Link} from "react-router-dom";
          <div className='rectangle_footer'>
              <div className={this.state.currentActive == 'timeline' ? 'ft_home-field active-timeline' : 'ft_home-field'}>
                  <Link to={`/home`}> <div className='timeline'>
-                     <span>{this.state.currentActive == 'timeline' ? 'Home' : ''}</span></div></Link>
+                     <span>{this.state.currentActive == 'timeline' ? <div className='center-text'><span className='text10-white'>Home</span></div>: ''}</span></div></Link>
              </div>
              <div className={this.state.currentActive == 'ball' ? 'ft_countries-field active' : 'ft_countries-field'}>
                  <Link to={`/countries`}> <div className='ball'>
-                     <span>{this.state.currentActive == 'ball' ? 'Sport' : ''}</span></div></Link>
+                     <span>{this.state.currentActive == 'ball' ? <div className='center-text'><span className='text10-white'>Sport</span></div> : ''}</span></div></Link>
              </div>
              <div className={this.state.currentActive == 'star' ? 'ft_star-field active' : 'ft_star-field'}>
                  <Link to={`/user_favourites`}> <div className='star'>
-                     <span>{this.state.currentActive == 'star' ? 'Favorites' : ''}</span></div></Link>
+                         <span>{this.state.currentActive == 'star' ? <div className='center-text'><span className='text10-white'>Favorites</span></div> : ''}</span></div></Link>
              </div>
              <div className={this.state.currentActive == 'profile' ? 'ft_profile-field active-profile' : 'ft_profile-field'}>
                  <Link to={`/profile`}> <div className='profile'>
-                     <span>{this.state.currentActive == 'profile' ? 'Profile' : ''}</span></div></Link>
+                             <span>{this.state.currentActive == 'profile' ? <div className='center-text'><span className='text10-white'>Profile</span></div> : ''}</span></div></Link>
              </div>
          </div>
      )
