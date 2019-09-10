f63ad04c5ec9a42349b7f7847a852f42b3f855e5
dusanugrcic <d.ugrcic@nix.mk>
2019-09-10 11:33:28 +0200

    Revert "screens"
    This reverts commit e3c4a497e82820fc56068b040eb534bf4f09f20d.

--- src/js/user/Login.js
+++ src/js/user/Login.js
@@ -55,19 +55,20 @@ class Login extends React.Component {
         return (<div className='betbook-screen-login'>
                 <div className='main-container'>
                     <div className='betbook-logo-box'><img src='./assets/images/betbook---logo.png' alt=''/></div>
-                    <div className='bs-email-container'>
+                    <div className='login-container'>
+                        <div className='bs-email-container'>
                         <div className='bs-email-text'><span className='text15-white'>Username</span></div>
                         <input className={this.state.validPassword ? 'bs-email-box' : 'bs-email-box bs-email-box-error'} type='username' value={this.state.username} onChange={this.handleChangeUsername}/>
                     </div>
                     <div className='bs-password-container'>
                         <div className='bs-password-text'><span className='text15-white'>Password</span></div>
                         <input className={this.state.validPassword ? 'bs-password-box' : 'bs-password-box bs-password-box-error'} type='password' value={this.state.password} onChange={this.handleChangePassword}/>
-                        <Link to={`/forgot-password`}><div className='bs-text-under-password'><span
-                            className='text11-white'>I forgot my password. </span></div></Link>
+                        <Link to={`/forgot-password`}><div className='bs-text-under-password'><span className='text11-white'>I forgot my password. </span></div></Link>
                     </div>
                     <div className='bs-create-account-box' onClick={() => this.handleLogin()}><span className='text18-white'>Sign in</span></div>
                     <Link to={`/register`}><div className='bs-i-already-have-an-account-box'><span className='text14-white'>I don't have an account.</span>
                     </div></Link>
+                    </div>
                 </div>
             </div>
         )
