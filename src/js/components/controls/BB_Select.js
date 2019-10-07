a3d12601094255610ce039bdc20f868723c199b4
dusanugrcic <d.ugrcic@nix.mk>
2019-10-07 17:21:13 +0200

    Design update

--- src/js/components/controls/BB_Select.js
+++ src/js/components/controls/BB_Select.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../../../style/components/controls/bb_select.scss'
 
 function BB_Select(props) {
 
-    let inputCssClass = "bs-email-box";
-    let clubsCssClass = 'bs-email-container';
+    let inputCssClass = "bb_sl-input-text";
+    let clubsCssClass = 'bb_sl_container';
 
     return <div className={clubsCssClass}>
-        <div className='bs-email-text'>{<span className='text15-white'>{props.text}</span>}</div>
+        <div className='bb_sl-text'>{<span className='text15-white'>{props.text}</span>}</div>
     <select className={inputCssClass} onChange={props.onChange} defaultValue={props.defaultValue}>
         {props.options.map(option=> <option value={option.value} key={option.key} defaultValue={props.defaultValue}>{option.label}</option>)}
     </select>
