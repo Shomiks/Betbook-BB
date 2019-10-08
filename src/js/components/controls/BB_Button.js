1604a3cbe6422f51ff416e6baa7818b598b3cffc
dusanugrcic <d.ugrcic@nix.mk>
2019-10-08 11:07:10 +0200

    Design update

--- src/js/components/controls/BB_Button.js
+++ src/js/components/controls/BB_Button.js
@@ -5,7 +5,7 @@ import '../../../style/components/controls/bb_button.scss'
 function BB_Button(props) {
 
     return <div className='bb_button' onClick={props.onClick}>
-        <div className='bs-email-text'><span className='text18-white'>{props.label}</span></div>
+        <span className='text18-white'>{props.label}</span>
     </div>
 }
 
